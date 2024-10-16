f601f954ba854bdc334f8fec2316bdab1489233f
1111234040 <n0729a3@gmail.com>
2024-10-16 16:03:00 +0800

    作業

---
+++ 1111234040/w6-2.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	float a, b, pi;
+	int i;
+	scanf_s("%f", &a);
+	i = 1;
+	b = 1;
+	pi = 4;
+	while (i <= a)
+	{
+		if (i % 2 == 0) {
+			pi += 4 / (1 + b * 2);
+		}
+		else {
+			pi -= 4 / (1 + b * 2);
+		}
+		i++;
+		b++;
+	}
+	printf("%f", pi);
+	return 0;
+}
