e7ddd4cb08e216096b9bfeb47032d45412ef3ee2
yuzhan024 <s1111234024@gmail.com>
2024-10-23 16:04:32 +0800

    Add files via upload

---
+++ 1111234024/WEEK7/WEEK7-1/WEEK7-1/024.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n ;
+    printf("請輸入n值（展開項數）：") ;
+    scanf_s("%d", &n) ;
+
+    double e = 1.0; 
+    double j = 1.0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        j = j/ i ; 
+        e += j ;
+    }
+
+    printf("%f\n", e) ;
+    return 0 ;
+}
---
+++ 1111234024/WEEK7/WEEK7-3/WEEK7-3/Source.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+int fact(int x) 
+{
+    int result = 1 ;
+    for (int i = 1 ; i <= x; i++) 
+    {
+        result *= i ;
+    }
+    return result ;
+}
+
+
+double e (int z)
+{
+    double sum = 1.0 ; 
+    for (int i = 1 ; i <= z; i++)
+    {
+        sum += 1.0 / fact(i) ; 
+    }
+    return sum ;
+}
+
+int main() 
+{
+    int z;
+    printf("請輸入Z值（項目數量）：") ;
+    scanf_s("%d", & z) ;
+
+ 
+    printf("近似的e值%f\n", e(z)) ;
+
+    return 0;
+}
