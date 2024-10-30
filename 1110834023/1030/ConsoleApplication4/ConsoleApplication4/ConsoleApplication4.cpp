5ad14a11e9342f42c2daa13da9aaa5791236a496
kyoki <135029618+kyoki618@users.noreply.github.com>
2024-10-30 16:09:29 +0800

    Add files via upload

---
+++ 1110834023/1030/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int rollDice(void);
+
+int main(void) {
+    int ex;
+    int frequency[13] = { 0 }; 
+
+    printf("輸入次數: ");
+    scanf_s("%d", &ex);
+
+    srand(time(NULL));
+
+
+    for (int i = 0; i < ex; ++i) {
+        int sum = rollDice();
+        frequency[sum]++;
+    }
+
+    printf("%s%13s\n", "點", "出現幾次");
+    for (int i = 2; i <= 12; ++i) {
+        printf("%4d%13d\n", i, frequency[i]);
+    }
+}
+
+int rollDice(void) {
+    int die1 = 1 + (rand() % 6); 
+    int die2 = 1 + (rand() % 6);
+    return die1 + die2;
+}
---
+++ 1110834023/1030/ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+long long power(int n, int k); 
+
+int main(void) {
+    int n, k;
+
+    printf("輸入 n: ");
+    scanf_s("%d", &n);
+
+    printf("輸入指數 k: ");
+    scanf_s("%d", &k);
+
+   
+    long long result = power(n, k);
+
+    printf("%d 的 %d 次方為: %lld\n", n, k, result);
+}
+
+long long power(int n, int k) {
+    if (k == 0) {
+        return 1;
+    }
+    else {
+        return n * power(n, k - 1);
+    }
+}
