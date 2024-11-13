c2839d642bdc1d36e67a7284593619b6efc70420
kyoki <135029618+kyoki618@users.noreply.github.com>
2024-11-13 15:44:24 +0800

    Add files via upload

---
+++ 1110934033/1113/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,23 @@
+﻿// Fig. 6.3: fig06_03.c
+// 手動輸入陣列元素並輸出
+#include <stdio.h>
+
+int main() {
+    int n[5]; 
+
+    printf("請輸入5個數：\n");
+   
+    for (size_t i = 0; i < 5; ++i) {
+        printf("請輸入第 %lu 個數的值: ", i);
+        scanf_s("%d", &n[i]); 
+    }
+
+    printf("第\n個數 \t值\n");
+
+  
+    for (size_t i = 0; i < 5; ++i) {
+        printf("%lu\t\t%d\n", i, n[i]);
+    }
+
+    return 0;
+}
