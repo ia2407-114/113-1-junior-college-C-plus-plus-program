a92642cffb55a14f29e1e339949d3f0ebbf2f5b5
andyliu300111 <andyliu300111@gmail.com>
2024-11-27 15:48:45 +0800

    week12

---
+++ 1111234038/week12/week12_001/001.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+double avg_score(int student[5][4], char names[5][10], int len);
+
+void main(void)
+{
+	// student[學生人數][學號, 計概, 數學, 平均]
+	int student[5][4] = { 0 };
+	char names[5][10] = {""};
+	int i, j;
+	double average = 0.0;
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("請輸入第%d位同學的姓名：", i + 1);
+		scanf_s("%s", names[i], 10);
+
+		printf("請輸入第%d位同學的學號：", i + 1);
+		scanf_s("%d", &student[i][0]);
+
+		printf("請輸入第%d位同學的計概成績：", i + 1);
+		scanf_s("%d", &student[i][1]);
+
+		printf("請輸入第%d位同學的數學成績：", i + 1);
+		scanf_s("%d", &student[i][2]);
+	}
+
+	average = avg_score(student, names, 5);
+
+	printf("班級平均：%lf\n", average);
+
+}
+
+double avg_score(int student[5][4], char names[5][10], int len)
+{
+	int i;
+	double total = 0.0;
+	for (i = 0; i < len; i++)
+	{
+		student[i][3] = (student[i][1] + student[i][2]) / 2;
+		total += student[i][3];
+	}
+
+	printf("\n%s%8s%8s%8s%8s\n", "姓名", "學號", "計概", "數學", "平均");
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("%s%10d%10d%10d%10d\n", names[i], student[i][0], student[i][1], student[i][2], student[i][3]);
+	}
+
+	return (total / len);
+}
