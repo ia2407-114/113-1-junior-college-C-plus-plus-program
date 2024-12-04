1afcd7224340f9abf754f6079aeba2c9b3456bdf
Katherine-wq <kate.cake.0712@gmail.com>
2024-12-04 15:58:38 +0800

    Add files via upload

---
+++ 1111234002/1113-第2題/1113-第二題/Source.cpp
@@ -0,0 +1,45 @@
+/*題目2. 輸入實驗次數ex，給定骰子的數量2，
+建立一個副程式，傳回擲骰子後的總點數。
+最後由主程式的陣列統計出各點出現的次數。*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int a(void); // 模擬擲兩顆骰子並返回總和
+void zu(int f[]); // 顯示柱狀圖
+int main(void) {
+     int f[13] = { 0 };
+     int ex; // 實驗次數
+
+    // 輸入實驗次數
+    printf("輸入實驗次數: ");
+    scanf_s("%d", &ex); 
+
+    // 執行擲骰子的實驗
+    for ( int i = 0; i < ex; ++i) {
+        // int total = a(); // 擲骰子並獲得總和
+        f[a()]++; // 根據總和更新頻率
+    }
+    // 顯示柱狀圖
+    zu(f);
+
+    return 0;
+}
+
+// 模擬擲兩顆骰子並返回總和
+int a(void) {
+    int d1 = 1 + (rand() % 6); 
+    int d2 = 1 + (rand() % 6); 
+    return d1 + d2; 
+}
+
+// 函數實現：顯示柱狀圖
+void zu(int f[]) {
+    for (int i = 2; i <= 12; ++i) { // 點數範圍是2到12
+        printf("%d: %u ", i, f[i]); // 顯示每個總和的出現次數
+        for ( int j = 0; j < f[i]; ++j) {
+            printf("*"); // 每個頻率對應一個星號
+        }
+        printf("\n");
+    }
+}
