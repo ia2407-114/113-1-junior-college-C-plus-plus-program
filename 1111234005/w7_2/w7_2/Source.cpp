92a10e955b06823ec62e49662144ce771ed20a01
1111234005-5 <s1111234005@nutc.edu.tw>
2024-12-11 13:37:28 +0800

    Add files via upload

---
+++ 1111234005/w7_2/w7_2/Source.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+double exponen(int);
+double const_e = 1.0;
+
+void main(void)
+{
+	int n;
+	double res;
+	printf("請輸入n的項數值:");
+	scanf_s("%d", n);
+
+	res = exponen(n);
+
+	printf("e的質為:%f", res);
+
+}
+double exponen(int n)
+{
+	int i;
+	double j = 1.0;
+	for (i = 1; i <= n; i++)
+	{
+		j *= i;
+		const_e += 1 / j;
+	}
+	return const_e;
+}
