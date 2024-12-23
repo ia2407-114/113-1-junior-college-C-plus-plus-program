947f41dfc0300724cd0eca23164d87a0485ddfa6
1111234025 <0966311315a@gmail.com>
2024-12-23 17:21:57 +0800

    Add files via upload

---
+++ 1111234025/w7_ex1/w7_ex1/Source.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int i, n;
+	double const_e = 1.0, j = 1.0;
+	printf("請輸入項目數量==> \n");
+	scanf_s("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		j *= i;
+		const_e += (1 / j);
+	}
+	printf("計算結果:%lf\n", const_e);
+	return 0;
+}
