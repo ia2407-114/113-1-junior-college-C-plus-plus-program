e6fb9c7358c8cb9ec144f7ee0efc8c0879f2de9b
1111234025 <0966311315a@gmail.com>
2024-12-23 17:18:25 +0800

    Add files via upload

---
+++ 1111234025/w6_ex1/w6_ex1/Source.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+int main()
+{
+	int n = 1;
+	int a = 1;
+
+	printf("½Ð¿é¤J³Ì¤j­È==> \n");
+	scanf_s("%d", &n);
+	while (a <= n)
+	{
+		printf("%d\t%d\n", a, a * a);
+		a++;
+	}
+	return 0;
+}
