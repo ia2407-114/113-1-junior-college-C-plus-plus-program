e249d72f81a8725cc660f3756f37e282a2c1eed4
s1111234023 <hong.30853091@gmail.com>
2025-01-06 14:51:59 +0800

    Update w11-1.cpp

--- 1111234023/w11/w11-1.cpp
+++ 1111234023/w11/w11-1.cpp
@@ -1,37 +1,39 @@
 #include <stdio.h>
 
-double avg_score(intarr[4][5],int1en);
+double avg_score(int a[4][5], int l);
+
 
 int main(void)
 {
 	int score[4][5] = { 0 };
-	int i = 0;
-	int avg = 0.0;
+	int j = 0;
+	double avg = 0.0;
 
-	for (i = 0;i <= 4;i++)
 
-	printf("輸入學號: \n");
-	scanf_s("%d", &score[0][1]);
-	printf("輸入計概成績: \n");
-	scanf_s("%d", &score[1][i]);
-	printf("輸入數學成績: \n");
-	scanf_s("%d", &score[2][i]);
-}
-
-totalav = avg_score(score, 5);
-printf("\n");
-printf("全班成績表如下\n");
-	int f = 0;
-	int b = 0;
-	for (b=0 ; b<= 3 ; b++)
+	for (j = 0; j <= 4; j++)
 	{
-		for (f = 0; f <= 4; f++)
+		printf("輸入學生學號\n");
+		scanf_s("%d", &score[0][j]);
+		printf("輸入學生計概\n");
+		scanf_s("%d", &score[1][j]);
+		printf("輸入學生數學\n");
+		scanf_s("%d", &score[2][j]);
+	}
+
+	avg = avg_score(score, 5);
+	printf("\n");
+	printf("全班成績表如下\n");
+	int e = 0;
+	int q = 0;
+	for (q = 0; q <= 3; q++)
+	{
+		for (e = 0; e <= 4; e++)
 		{
-			printf("%d", score[b][f]);
+			printf("%d		", score[q][e]);
 		}
 		printf("\n");
 	}
-	printf("全班總成績平均為:%f\n", avg);
+	printf("全班總成績平均為:%lf\n", avg);
 }
 
 double avg_score(int a[4][5], int l)
@@ -44,7 +46,7 @@ double avg_score(int a[4][5], int l)
 		a[3][i] = (a[1][i] + a[2][i]) / 2;
 	}
 
-	ttav = (a[3][0] + a[3][1] + a[3][2] + a[3][3] + a[3][4]) / 5;
+	avg = (a[3][0] + a[3][1] + a[3][2] + a[3][3] + a[3][4]) / 5;
 
 	return avg;
 }
