bf5262e95404b86356243cad5c959b9b81338d60
1111234018 <adam081488@gmail.com>
2025-03-05 15:51:44 +0800

    Add files via upload

---
+++ 1111234018/W2-2.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+struct studentData {
+    unsigned int ID;
+    char Name[15]; 
+    double math; 
+    double computer;
+    double average;
+};
+
+int main(void)
+{
+    FILE* sfPtr; 
+
+  
+    if (fopen_s(&sfPtr, "students.dat", "rb") != 0) {
+        puts("File could not be opened.");
+    }
+    else {
+        printf("%-6s%-16s%-10s%-10s%10s\n", "ID", "Name",
+            "Math", "Computer", "Average");
+
+       
+        while (!feof(sfPtr)) {
+           
+            struct studentData student = { 0, "", 0.0, 0.0, 0.0 };
+
+            int result = fread(&student, sizeof(struct studentData), 1, sfPtr);
+
+           
+            if (result != 0 && student.ID != 0) {
+                printf("%-6d%-16s%-10.2f%-10.2f%10.2f\n",
+                    student.ID, student.Name,
+                    student.math, student.computer, student.average);
+            }
+        }
+
+        fclose(sfPtr); 
+    }
+}
