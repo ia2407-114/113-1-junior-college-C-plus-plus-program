4044ab298c4b748cde3ab0742772984ffc18305b
1111234033 <xiaoen970201@gmail.com>
2025-03-26 15:45:07 +0800

    Add files via upload

---
+++ 1111234033/W6-1+2/Project1/Source.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+
+class D
+{
+public:
+	explicit
+	D(string n, int a)
+	{
+		name = n;
+		age = a;
+	}
+
+	void setage(int a)
+	{
+		if (a < 20 && a >= 1)
+		{
+			age = a;
+
+		}
+		else
+		{
+			cout << "資料有誤不得大於20    ";
+		}
+
+	}
+
+	int getage()
+	{
+		return age;
+	}
+	void setname(string n)
+	{
+		name = n;
+	}
+	string getname()
+	{
+		return name;
+	}
+	void display()
+	{
+		cout << "dog age :" << getage() << endl;
+		cout << "dog name :" << getname() << endl;
+
+	}
+
+private:
+	int age;
+	string name;
+
+};
+
+int main()
+{
+	D dog("hi", 5);
+	int a;
+	string n;
+
+	cout << "input age: ";
+	cin >> a;
+
+	cout << "input name: ";
+	cin >> n;
+
+
+	dog.setage(a);
+	dog.setname(n);
+	dog.display();
+
+}
